72dc6de9541847d56c8a7c19372a07d0caed211d
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-03-10 09:15:46 -0300

    Validando CPF existente

--- finAPI/src/index.js
+++ finAPI/src/index.js
@@ -17,10 +17,18 @@ const customers = [];
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
 
-    const id = uuidv4();
+    const customerAlreadyExists = customers.some(
+        (customers) => customers.cpf === cpf
+    );
+
+    if(customerAlreadyExists) {
+        return response.status(400).json({
+            error: "Customer already exists!"
+        })
+    }
 
     customers.push({
-        id,
+        id: uuidv4(),
         cpf,
         name,
         statement: []
