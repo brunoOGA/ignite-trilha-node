1ca1e7fd694875b9eb86b09b6ac3b9306e9a20ee
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-03-10 09:40:29 -0300

    Middlewares

--- finAPI/src/index.js
+++ finAPI/src/index.js
@@ -7,6 +7,23 @@ app.use(express.json());
 
 const customers = [];
 
+//Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+    
+    if(!customer) {
+        return response.status(400).json({
+            error: 'Customer not found'
+        });
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
 /**
  * cpf - string
  * name - string
@@ -37,17 +54,11 @@ app.post("/account", (request, response) => {
     return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-    const { cpf } = request.headers;
+//app.use(verifyIfExistsAccountCPF);
 
-    const customer = customers.find(customer => customer.cpf === cpf);
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
     
-    if(!customer) {
-        return response.status(400).json({
-            error: 'Customer not found'
-        });
-    }
-
     return response.json(customer.statement);
 });
 
