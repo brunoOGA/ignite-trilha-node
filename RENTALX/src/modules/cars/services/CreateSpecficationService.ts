3e357ca732323a5558af8391eca9200bd8e4fbfe
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-03-16 15:28:25 -0300

    Criando repositório de especificação

--- RENTALX/src/modules/cars/services/CreateSpecficationService.ts
+++ RENTALX/src/modules/cars/services/CreateSpecficationService.ts
@@ -1,3 +1,27 @@
-class CreateSpecficationService {}
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecficationService {
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists = this.specificationsRepository.findByName(
+      name
+    );
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists!");
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
+  }
+}
 
 export { CreateSpecficationService };
---
+++ RENTALX/src/routes/spefications.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+
+import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
+import { CreateSpecficationService } from "../modules/cars/services/CreateSpecficationService";
+
+const specificationsRoutes = Router();
+const specificationsRepository = new SpecificationsRepository();
+
+specificationsRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+  const createSpecificationService = new CreateSpecficationService(
+    specificationsRepository
+  );
+
+  createSpecificationService.execute({ name, description });
+
+  return response.status(201).send();
+});
+
+export { specificationsRoutes };
