bb63c6404886ab7d482303f3a004dcbc30645914
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-04-21 08:23:50 -0300

    Refatorando as especificações && Criando migration de especificação && Continuação da documentação

--- RENTALX/src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ RENTALX/src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -4,14 +4,14 @@ import { container } from "tsyringe";
 import { CreateSpecficationUseCase } from "./CreateSpecificationUseCase";
 
 class CreateSpecificationController {
-  handle(request: Request, response: Response): Response {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
 
     const createSpecificationUseCase = container.resolve(
       CreateSpecficationUseCase
     );
 
-    createSpecificationUseCase.execute({ name, description });
+    await createSpecificationUseCase.execute({ name, description });
 
     return response.status(201).send();
   }
