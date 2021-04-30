d9e0f8ce585e17f6623be8ba3e4f214b358e6ce2
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-04-30 15:31:45 -0300

    Criando migrations de imagens de carro

--- RENTALX/src/shared/infra/http/routes/cars.routes.ts
+++ RENTALX/src/shared/infra/http/routes/cars.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { CreateCarSpeficationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
-import { ListAvailableCarsController } from "@modules/cars/useCases/ListAvailableCars/ListAvailableCarsController";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
