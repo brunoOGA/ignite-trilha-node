de51f6fa77ae7480883d22a93459e61b546adf4b
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-05-01 09:58:26 -0300

    Criando migrations do aluguel

--- RENTALX/src/shared/infra/typeorm/migrations/1619004331800-CreateUsers.ts
+++ RENTALX/src/shared/infra/typeorm/migrations/1619004331800-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1619004331800 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
