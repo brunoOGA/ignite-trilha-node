dc6536f2305059dd6986fa6f296905b156d2cfc3
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-05-02 17:14:22 -0300

    Continuação do cadastro de aluguel

--- RENTALX/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ RENTALX/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 
 import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
@@ -11,11 +12,46 @@ describe("Create Rental", () => {
     createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
   });
 
-  it("shoould be able to create a new rental", async () => {
-    await createRentalUseCase.execute({
+  it("should be able to create a new rental", async () => {
+    const rental = await createRentalUseCase.execute({
       user_id: "12345",
       car_id: "121212",
       expected_return_date: new Date(),
     });
+
+    expect(rental).toHaveProperty("id");
+    expect(rental).toHaveProperty("start_date");
+  });
+
+  it("should not be able to create a new rental if there is another open to same user", async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: "user_id_1",
+        car_id: "car_id_1",
+        expected_return_date: new Date(),
+      });
+
+      await createRentalUseCase.execute({
+        user_id: "user_id_1",
+        car_id: "car_id_2",
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to create a new rental if there is another open to same car", async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: "user_id_1",
+        car_id: "car_id_1",
+        expected_return_date: new Date(),
+      });
+
+      await createRentalUseCase.execute({
+        user_id: "user_id_2",
+        car_id: "car_id_1",
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
