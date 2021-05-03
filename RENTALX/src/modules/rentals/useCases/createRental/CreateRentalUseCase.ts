e6e94ae9877b1931cf315e0b836c1666ac669c9c
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-05-03 09:19:14 -0300

    Criando provider para data

--- RENTALX/src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ RENTALX/src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,8 +1,6 @@
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
@@ -11,10 +9,11 @@ interface IRequest {
   expected_return_date: Date;
 }
 
-dayjs.extend(utc);
-
 class CreateRentalUseCase {
-  constructor(private rentalsRepository: IRentalsRepository) {}
+  constructor(
+    private rentalsRepository: IRentalsRepository,
+    private dateProvider: IDateProvider
+  ) {}
 
   async execute({
     user_id,
@@ -39,14 +38,10 @@ class CreateRentalUseCase {
       throw new AppError("There's a rental in progress for user!");
     }
 
-    const expectedReturnDateFormat = dayjs(expected_return_date)
-      .utc()
-      .local()
-      .format();
-
-    const dateNow = dayjs().utc().local().format();
-
-    const compare = dayjs(expectedReturnDateFormat).diff(dateNow, "hours");
+    const compare = this.dateProvider.compareInHours(
+      new Date(),
+      expected_return_date
+    );
 
     if (compare < minimumHour) {
       throw new AppError("Invalid return time!");
---
+++ RENTALX/src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -0,0 +1,17 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
+import { IDateProvider } from "../IDateProvider";
+
+dayjs.extend(utc);
+
+class DayjsDateProvider implements IDateProvider {
+  compareInHours(start_date: Date, end_date: Date): number {
+    const end_date_utc = dayjs(end_date).utc().local().format();
+    const start_date_utc = dayjs(start_date).utc().local().format();
+
+    return dayjs(end_date_utc).diff(start_date_utc, "hours");
+  }
+}
+
+export { DayjsDateProvider };
