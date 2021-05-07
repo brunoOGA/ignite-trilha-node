36419c0b6b1caf8310c3b9fb23eae2a130312bd2
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-05-07 09:56:20 -0300

    Refresh token

---
+++ RENTALX/src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "daa308c1587f95140f645b796e04c810",
+  expires_in_token: "15m",
+  secret_refresh_token: "ee57d7f624660da494d99587d970849c",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
--- RENTALX/src/shared/container/providers/DateProvider/IDateProvider.ts
+++ RENTALX/src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -2,6 +2,7 @@ interface IDateProvider {
   compareInHours(start_date: Date, end_date: Date): number;
   dateNow(): Date;
   compareInDays(start_date: Date, end_date: Date): number;
+  addDays(days: number): Date;
 }
 
 export { IDateProvider };
