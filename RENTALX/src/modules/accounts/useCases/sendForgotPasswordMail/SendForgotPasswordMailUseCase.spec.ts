fcacd7a4bd8e8de7f245f86535ac15384456ac0e
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-05-09 19:49:45 -0300

    Testando envio de e-mail

---
+++ RENTALX/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.spec.ts
@@ -0,0 +1,64 @@
+import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
+import { UsersTokensRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory";
+import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
+import { MailProviderInMemory } from "@shared/container/providers/MailProvider/in-memory/MailProviderInMemory";
+import { AppError } from "@shared/errors/AppError";
+
+import { SendForgotPasswordMailUseCase } from "./SendForgotPasswordMailUseCase";
+
+let sendForgotPasswordMailUseCase: SendForgotPasswordMailUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let dateProvider: DayjsDateProvider;
+let usersTokensRepositoryInMemory: UsersTokensRepositoryInMemory;
+let mailProvider: MailProviderInMemory;
+
+describe("Send forgot mail", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    usersTokensRepositoryInMemory = new UsersTokensRepositoryInMemory();
+    dateProvider = new DayjsDateProvider();
+    mailProvider = new MailProviderInMemory();
+    sendForgotPasswordMailUseCase = new SendForgotPasswordMailUseCase(
+      usersRepositoryInMemory,
+      usersTokensRepositoryInMemory,
+      dateProvider,
+      mailProvider
+    );
+  });
+
+  it("should be able to send a forgot password mail to user", async () => {
+    const sendMail = spyOn(mailProvider, "sendMail");
+
+    await usersRepositoryInMemory.create({
+      driver_license: "057834",
+      email: "woruk@fuw.tj",
+      name: "Kevin Collins",
+      password: "1234",
+    });
+
+    await sendForgotPasswordMailUseCase.execute("woruk@fuw.tj");
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+
+  it("should not be able to send a forgot password mail if user does not exists", async () => {
+    await expect(
+      sendForgotPasswordMailUseCase.execute("esuwame@rawo.hm")
+    ).rejects.toEqual(new AppError("User does not exists!"));
+  });
+
+  it("should be able to create an users token", async () => {
+    const generateTokenMail = spyOn(usersTokensRepositoryInMemory, "create");
+
+    await usersRepositoryInMemory.create({
+      driver_license: "649923",
+      email: "vu@otu.ve",
+      name: "Don Boyd",
+      password: "1234",
+    });
+
+    await sendForgotPasswordMailUseCase.execute("vu@otu.ve");
+
+    expect(generateTokenMail).toBeCalled();
+  });
+});
