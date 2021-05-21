69e4b6476a28a2d1fd4e20a6d545990ac42e844c
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-05-21 09:23:44 -0300

    Criando o template com handlebars

--- 05 - ignitecertificate/src/functions/generateCertificate.ts
+++ 05 - ignitecertificate/src/functions/generateCertificate.ts
@@ -1,9 +1,31 @@
-import {document} from '../utils/dynamodbClient';
+import * as chromium from "chrome-aws-lambda";
+import * as path from "path";
+import * as fs from "fs";
+import * as handlebars from "handlebars";
+import * as dayjs from "dayjs";
+
+import {document} from "../utils/dynamodbClient";
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
+}
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  date: string;
+  medal: string;
+}
+
+const compile = async function(data: ITemplate) {
+  const filePath = path.join(process.cwd(), "src", "templates", "certificate.hbs");
+
+  const html = fs.readFileSync(filePath, "utf-8");
+
+  return handlebars.compile(html)(data);
 }
 
 export const handle = async (event) => {
@@ -17,6 +39,22 @@ export const handle = async (event) => {
       grade
     }
   }).promise();
+
+  const medalPath = path.join(process.cwd(), "src", "templates", "selo.png");
+  const medal = fs.readFileSync(medalPath, "base64");
+
+
+  const data: ITemplate = {
+    date: dayjs().format("DD/MM/YYYY"),
+    grade,
+    name,
+    id,
+    medal
+  }
+
+  const content = await compile(data);
+
+
 
   return {
     statusCode: 201,
